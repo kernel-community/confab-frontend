64c19790c18b5f2922e3fd75763c125f7ec8bf53
Angela Gilhotra <angelagilhotra@gmail.com>
2021-09-29 03:13:41 +0530

    fix banner

--- components/BigFatBanner.tsx
+++ components/BigFatBanner.tsx
@@ -1,13 +1,11 @@
 const BigFatBanner = ({text, helperText}: {text: string, helperText?:string}) => {
   return (
     <div className="
-        w-full h-96 relative
         rounded-lg
         bg-gradient-to-r from-gray-700 via-gray-900 to-black
       ">
       <div className="
-          absolute bottom-9 left-14
-          font-inter font-bold text-6xl
+          font-inter font-bold text-6xl p-32
           text-gray-200
         ">
         {text}
