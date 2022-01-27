d070df05b3937d79a20e0bfb59a7f91662cc44fb
Angela Gilhotra <angelagilhotra@gmail.com>
2022-01-27 00:20:50 -0400

    add seo to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,39 @@
 import type {NextPage} from 'next';
 import Main from '../layouts/Main';
 import {Events} from '../components/composed/Events';
+import {NextSeo} from 'next-seo';
 
 const Home: NextPage = () => {
   return (
-    <Main>
-      <Events
-        type='live'
+    <>
+      <NextSeo
+        title='Home'
+        openGraph={{
+          url: `https://convo.kernel.community`,
+          title: `KERNEL Convo`,
+          description: 'A home for conversations taking place in the Kernel network',
+          images: [
+            {
+              url: 'https://confab-frontend.vercel.app/images/banner.jpg',
+              alt: 'KERNEL squares and circles',
+              type: 'image/jpeg',
+            },
+          ],
+          site_name: 'KERNEL Convo',
+        }}
       />
-      <Events
-        type='upcoming'
-      />
-      <Events
-        type='past'
-      />
-    </Main>
+      <Main>
+        <Events
+          type='live'
+        />
+        <Events
+          type='upcoming'
+        />
+        <Events
+          type='past'
+        />
+      </Main>
+    </>
   );
 };
 export default Home;
