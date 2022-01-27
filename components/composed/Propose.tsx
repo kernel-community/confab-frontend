94462148badaf38e2ff91523ddca139aca69c044
Angela Gilhotra <angelagilhotra@gmail.com>
2022-01-27 01:12:49 -0400

    refactor

--- components/composed/Propose.tsx
+++ components/composed/Propose.tsx
@@ -1,12 +1,10 @@
 /* eslint-disable no-invalid-this */
 import Text from '../atomic/Text';
 import SessionsInput from '../form/SessionsInput';
-import TextArea from '../atomic/TextArea';
 import NumberComponent from '../atomic/Number';
 import EventType from '../form/EventType';
 import Button from '../atomic/Button';
-import {ReactElement, useEffect, useState} from 'react';
-// import {DateTime} from 'luxon';
+import {useEffect, useState} from 'react';
 import {useRouter} from 'next/router';
 import {noSpecialChars, onlyURL} from '../../utils/regex';
 import {sessionDatesValidity} from '../../utils';
@@ -41,36 +39,74 @@ const Propose = ({
   const [disableSubmit, setDisableSubmit] = useState<boolean>(false);
   const [titleValidation, setTitleValidation] = useState<{state: boolean, reason?: string}>({state: false, reason: ``});
   const [locationValidation, setLocationValidation] = useState<{state: boolean, reason?: string}>({state: false, reason: ``});
-  // const [limitValidation, setLimitValidation] = useState<{state: boolean, reason?: string}>({state: false, reason: ``});
+  const [limitValidation, setLimitValidation] = useState<{state: boolean, reason?: string}>({state: false, reason: ``});
   const [dateTimesValidation, setDateTimesValidation]=useState<boolean>(false);
-  useEffect(() => {
-    let disable: boolean = false;
-    if (
-      eventDetails.title?.length == 0 ||
-      eventDetails.location?.length == 0 ||
-      eventDetails.proposerEmail?.length == 0 ||
-      eventDetails.proposerName?.length == 0
-    ) {
-      disable = true;
+
+  /**
+   * Validate fields
+   */
+  //
+  const validateFields = (field: string, value: string) => {
+    switch (field) {
+      case 'title':
+        setTitleValidation({state: false, reason: ``});
+        if (noSpecialChars.test(value.toLowerCase())) {
+          setTitleValidation({state: true, reason: 'No special characters'});
+        } else {
+          setTitleValidation({state: false, reason: ``});
+        }
+        break;
+      case 'location':
+        setLocationValidation({state: false, reason: ``});
+        if (onlyURL.test(value.toLowerCase()) || value.toLowerCase().startsWith('irl')) {
+          setLocationValidation({state: false, reason: ``});
+        } else {
+          setLocationValidation({state: true, reason: 'Not a valid URL'});
+        }
+        break;
+      case 'limit':
+        setLimitValidation({state: false, reason: ``});
+        if (Number(value) > 90 || Number(value) < 0) {
+          setLimitValidation({state: true, reason: 'Positive integers less than 90 only :)'});
+        } else {
+          setLimitValidation({state: false, reason: ``});
+        }
+        break;
     }
-    if (
-      titleValidation.state || locationValidation.state
-      // || limitValidation.state
-    ) {
-      disable = true;
+  };
+
+  /**
+   * Handle all inputs
+   */
+  // handle input from all form objects
+  const handleInput = (e: any) => {
+    const target: 'title' | 'location' | 'limit' | 'proposerEmail' | 'proposerName' | 'eventType' = e.target.name;
+    switch (target) {
+      case 'title':
+      case 'location':
+      case 'proposerEmail':
+      case 'proposerName':
+      case 'limit':
+      case 'eventType':
+        setEventDetails(Object.assign(eventDetails, {
+          [target]: e.target.value,
+        }));
+        validateFields(target, e.target.value);
+        break;
+      default: return;
     }
-    setDisableSubmit(disable);
-  }, [
-    eventDetails.title,
-    eventDetails.location,
-    titleValidation,
-    locationValidation,
-    // limitValidation,
-  ]);
+  };
+  // handle description's richtext input from the form
+  const handleDescriptionInput = (e: any) => {
+    setEventDetails(Object.assign(eventDetails, {
+      'descriptionText': e.editor.getText(),
+      'descriptionHtml': e.editor.getHTML(),
+    }));
+  };
+  // handle single / multiple sessions (date time + duration) input
   const handleSessionsInput = (type: 'dateTime' | 'duration', count: number, e: any) => {
     switch (type) {
       case 'dateTime':
-        setDateTimesValidation(false);
         setSessionDetails((currentSessions) => {
           const currentSession = currentSessions.find((s) => s.count === count);
           if (currentSession) {
@@ -110,71 +146,25 @@ const Propose = ({
         break;
     }
   };
+  // handle deletion of a session while input
   const handleSessionDelete = (count: number) => {
     setSessionDetails((sessions) => sessions.filter((s) => s.count != count));
   };
+  // handle checking / unchecking the `is recurring` field on the form
+  // if unchecked, reset the component back to just the first session
   const resetSessions = (isRecurring: boolean) => {
-    if (!isRecurring) {
-      setSessionDetails((currentSessions) => [currentSessions[0]]);
-    }
+    if (isRecurring) return;
+    setSessionDetails((currentSessions) => [currentSessions[0]]);
   };
-  const validateFields = (field: string, value: string) => {
-    switch (field) {
-      case 'title':
-        if (noSpecialChars.test(value.toLowerCase())) {
-          setTitleValidation({state: true, reason: 'No special characters'});
-        } else {
-          setTitleValidation({state: false, reason: ``});
-        }
-        break;
-      case 'location':
-        if (onlyURL.test(value.toLowerCase()) || value.toLowerCase().startsWith('irl')) {
-          setLocationValidation({state: false, reason: ``});
-        } else {
-          setLocationValidation({state: true, reason: 'Not a valid URL'});
-        }
-        break;
-      case 'limit':
-        // if (Number(value) > 90 || Number(value) < 0) {
-        //   setLimitValidation({state: true, reason: 'Positive integers less than 90 only :)'});
-        // } else {
-        //   setLimitValidation({state: false, reason: ``});
-        // }
-        break;
-    }
-  };
-  const handleInput = (e: any) => {
-    const target: 'title' | 'location' | 'description' | 'limit' | 'proposerEmail' | 'proposerName' | 'eventType' = e.target.name;
-    switch (target) {
-      case 'title':
-      case 'location':
-      case 'proposerEmail':
-      case 'proposerName':
-      case 'limit':
-      case 'eventType':
-        setEventDetails(Object.assign(eventDetails, {
-          [target]: e.target.value,
-        }));
-        validateFields(target, e.target.value);
-        break;
-      case 'description':
-        setEventDetails(Object.assign(eventDetails, {
-          descriptionText: e.target.value,
-          descriptionHtml: '<p>' + e.target.value + '</p>',
-        }));
-      default: return;
-    }
-  };
-  const handleDescription = (e: any) => {
-    setEventDetails(Object.assign(eventDetails, {
-      'descriptionText': e.editor.getText(),
-      'descriptionHtml': e.editor.getHTML(),
-    }));
-  };
+
+
+  /**
+   * Handle form submission
+   */
+  //
   const handleSubmit = async (e: SubmitEvent) => {
-    console.log({event: eventDetails, sessions: sessionDetails});
     e.preventDefault();
-    if (!sessionDatesValidity(sessionDetails) && eventDetails.eventType != 3) {
+    if (!sessionDatesValidity(sessionDetails)) {
       setDateTimesValidation(true);
       return;
     }
@@ -217,6 +207,20 @@ const Propose = ({
     setLoading(false);
     setDisableSubmit(false);
   };
+
+
+  /**
+   * Handle disbaling the Submit button
+   */
+  //
+  useEffect(() => {
+    let disable = false;
+    setDisableSubmit(disable);
+    if (eventDetails.title?.length === 0 || eventDetails.location?.length === 0 || eventDetails.proposerEmail?.length === 0) disable = true;
+    if (limitValidation.state || titleValidation.state || locationValidation.state) disable = true;
+    setDisableSubmit(disable);
+  }, [limitValidation.state, titleValidation.state, locationValidation.state, eventDetails.title, eventDetails.location, eventDetails.proposerEmail]);
+
   return (
     <div className={className}>
       <div className="grid xl:grid-cols-2 gap-4 grid-cols-1">
@@ -225,15 +229,17 @@ const Propose = ({
         />
         <Text
           name="title"
-          fieldName="Title"
+          fieldName="Title *"
           handleChange={handleInput}
+          danger={titleValidation.state}
+          dangerReason={titleValidation.reason}
         />
 
         <FieldLabel styles='my-auto'>
           Description
         </FieldLabel>
 
-        <TipTap handleChange={handleDescription}/>
+        <TipTap handleChange={handleDescriptionInput}/>
 
         <SessionsInput
           handleChange={handleSessionsInput}
@@ -246,8 +252,8 @@ const Propose = ({
           fieldName="Set Limit"
           infoText={`Enter maximum number of seats for your session(s). Enter 0 for no limit`}
           handleChange={handleInput}
-          // danger={limitValidation.state}
-          // dangerReason={limitValidation.reason}
+          danger={limitValidation.state}
+          dangerReason={limitValidation.reason}
           placeholder="0"
         />
         <Text
@@ -262,7 +268,7 @@ const Propose = ({
         />
         <Text
           name="proposerEmail"
-          fieldName="Email"
+          fieldName="Email *"
           handleChange={handleInput}
           infoText={
             `
@@ -285,8 +291,6 @@ const Propose = ({
           />
         </div>
       </div>
-
-
     </div>
   );
 };
