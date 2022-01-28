d2be8fd8be02948b44fb7e909e815070f3df2499
Angela Gilhotra <angelagilhotra@gmail.com>
2022-01-28 16:49:14 -0400

    add navbar

---
+++ components/composed/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import {Item} from 'components/composed/Navbar/Item';
+import {Branding} from './Branding';
+
+export const Navbar = () => {
+  return (
+    <>
+      <div className={`
+    w-full flex flex-row gap-8 justify-center items-center bg-kernel-purple
+    font-secondary text-sm text-gray-300
+    px-3
+    `}>
+        <Branding />
+        <div className="flex-grow flex flex-row gap-8 items-center justify-center">
+          <Item text='home' href='/'/>
+          <Item text='all' href='/all'/>
+          <Item text='archive' href='/archive'/>
+          <Item text='about' href='/about'/>
+        </div>
+        <Item text='propose' href='/propose'/>
+      </div>
+    </>
+  );
+};
