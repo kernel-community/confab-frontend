5a0d284c5eb3be368893a55efd25a6cbd57dc23b
Angela Gilhotra <angelagilhotra@gmail.com>
2022-01-28 16:49:19 -0400

    add fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,6 +48,22 @@ function MyApp({Component, pageProps}: AppProps) {
             rel: 'stylesheet',
             href: 'https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:ital,wght@0,300;0,400;0,500;0,700;0,800;1,300;1,400;1,500;1,700;1,800&display=swap',
           },
+          {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&display=swap',
+          },
+          {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Licorice&display=swap',
+          },
+          {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap',
+          },
+          {
+            rel: 'preload',
+            href: '/public/fonts/Futura/futura-medium.ttf',
+          },
         ]}
       />
       <Component {...pageProps} />
