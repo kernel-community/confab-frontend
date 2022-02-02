8cd6624b9cddab7b57cbcc6341f9d816a3e8fa0f
Angela Gilhotra <angelagilhotra@gmail.com>
2022-02-01 22:16:07 -0400

    update tagline

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ const Home: NextPage = () => {
                 pt-12
               "
           >
-            Spark a thought,&nbsp;
+            {/* Spark a thought,&nbsp; */}
             <Link href='/propose'>
               <span className='before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-highlight relative inline-block cursor-pointer'>
                 <span className='relative text-primary'>
